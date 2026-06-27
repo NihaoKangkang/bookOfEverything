2057167e1e0ee6f57bfeba2dc2199483d70afded
Kyi Wong <48087921+NihaoKangkang@users.noreply.github.com>
2026-06-28 02:16:39 +0800

    Add files via upload

---
+++ answers_data.js
@@ -0,0 +1,1074 @@
+const ANSWERS_DATA = [
+  {
+    "cn": "寻求更多的选择",
+    "en": "Seek more choices"
+  },
+  {
+    "cn": "不",
+    "en": "No"
+  },
+  {
+    "cn": "别忽视显而易见的东西",
+    "en": "Don't overlook the obvious"
+  },
+  {
+    "cn": "结果可能令人吃惊",
+    "en": "The result may be surprising"
+  },
+  {
+    "cn": "有决心就能成功",
+    "en": "Determination will lead to success"
+  },
+  {
+    "cn": "做一次改变",
+    "en": "Make a change"
+  },
+  {
+    "cn": "照别人告诉你的去做",
+    "en": "Do as others tell you"
+  },
+  {
+    "cn": "不能保证",
+    "en": "No guarantee"
+  },
+  {
+    "cn": "答案可能会以另一种形式出现",
+    "en": "The answer may come in another form"
+  },
+  {
+    "cn": "毫无疑问",
+    "en": "Without a doubt"
+  },
+  {
+    "cn": "这样做会使事情变得有趣",
+    "en": "This will make things interesting"
+  },
+  {
+    "cn": "这是肯定的",
+    "en": "This is affirmative"
+  },
+  {
+    "cn": "有可能会伤害到他人",
+    "en": "It may harm others"
+  },
+  {
+    "cn": "全身心投入将赢得好结果",
+    "en": "Wholehearted devotion will bring good results"
+  },
+  {
+    "cn": "采取冒险的态度",
+    "en": "Take an adventurous attitude"
+  },
+  {
+    "cn": "最好等待",
+    "en": "Better to wait"
+  },
+  {
+    "cn": "可能会惹上麻烦",
+    "en": "May get into trouble"
+  },
+  {
+    "cn": "你需要采取主动",
+    "en": "You need to take the initiative"
+  },
+  {
+    "cn": "似乎没问题",
+    "en": "Seems ok"
+  },
+  {
+    "cn": "当然",
+    "en": "Of course"
+  },
+  {
+    "cn": "不要在意",
+    "en": "Don't mind"
+  },
+  {
+    "cn": "尽早做好它",
+    "en": "Do it soon"
+  },
+  {
+    "cn": "你终会发现你想知道的一切",
+    "en": "You will eventually find out what you want to know"
+  },
+  {
+    "cn": "除非你独自一人",
+    "en": "Unless you are alone"
+  },
+  {
+    "cn": "是，但不要强求",
+    "en": "Yes, but don't force it"
+  },
+  {
+    "cn": "你需要去适应",
+    "en": "You need to adapt"
+  },
+  {
+    "cn": "明天再来试试",
+    "en": "Try again tomorrow"
+  },
+  {
+    "cn": "以更放松的态度去面对",
+    "en": "Face it with a more relaxed attitude"
+  },
+  {
+    "cn": "你需要考虑其他方法",
+    "en": "You need to consider other ways"
+  },
+  {
+    "cn": "在习惯中接受一些改变",
+    "en": "Accept some changes in habits"
+  },
+  {
+    "cn": "要知道选择太多和选择太少一样很难",
+    "en": "Know that having too many or too few choices is equally difficult"
+  },
+  {
+    "cn": "别浪费时间了",
+    "en": "Don't waste time"
+  },
+  {
+    "cn": "是",
+    "en": "Yes"
+  },
+  {
+    "cn": "花更多时间来决定",
+    "en": "Take more time to decide"
+  },
+  {
+    "cn": "灵活应对",
+    "en": "Be flexible"
+  },
+  {
+    "cn": "似乎已成事实",
+    "en": "Seems to be a fact"
+  },
+  {
+    "cn": "看开一点",
+    "en": "Take it easy"
+  },
+  {
+    "cn": "问问你的异性同事",
+    "en": "Ask your opposite-sex colleague"
+  },
+  {
+    "cn": "柳暗花明又一村",
+    "en": "After the darkness there will be brightness"
+  },
+  {
+    "cn": "你会后悔的",
+    "en": "You will regret it"
+  },
+  {
+    "cn": "避免第一个解决办法",
+    "en": "Avoid the first solution"
+  },
+  {
+    "cn": "随它去吧",
+    "en": "Let it be"
+  },
+  {
+    "cn": "过段时间就不那么重要了",
+    "en": "It won't be so important after a while"
+  },
+  {
+    "cn": "拭目以待",
+    "en": "Wait and see"
+  },
+  {
+    "cn": "相信你最初的想法",
+    "en": "Trust your initial thoughts"
+  },
+  {
+    "cn": "请不要抗拒",
+    "en": "Please don't resist"
+  },
+  {
+    "cn": "这会带来好运",
+    "en": "This will bring good luck"
+  },
+  {
+    "cn": "那一定很棒",
+    "en": "That must be great"
+  },
+  {
+    "cn": "把它记下来",
+    "en": "Write it down"
+  },
+  {
+    "cn": "可能发生小意外",
+    "en": "A minor accident may occur"
+  },
+  {
+    "cn": "学习并享受它",
+    "en": "Learn and enjoy it"
+  },
+  {
+    "cn": "这具有重要意义",
+    "en": "This has important meaning"
+  },
+  {
+    "cn": "防备意外发生",
+    "en": "Beware of accidents"
+  },
+  {
+    "cn": "一切将依赖于你的选择",
+    "en": "Everything will depend on your choice"
+  },
+  {
+    "cn": "转移注意力",
+    "en": "Divert your attention"
+  },
+  {
+    "cn": "离开",
+    "en": "Leave"
+  },
+  {
+    "cn": "你需要其他人的帮助",
+    "en": "You need help from others"
+  },
+  {
+    "cn": "这有些特别",
+    "en": "This is a bit special"
+  },
+  {
+    "cn": "不要犹豫",
+    "en": "Don't hesitate"
+  },
+  {
+    "cn": "先完成其他事",
+    "en": "Finish other things first"
+  },
+  {
+    "cn": "给自己一点时间",
+    "en": "Give yourself some time"
+  },
+  {
+    "cn": "现在你就能",
+    "en": "You can do it now"
+  },
+  {
+    "cn": "那不值得纠结",
+    "en": "It's not worth worrying about"
+  },
+  {
+    "cn": "那将影响别人对你的看法",
+    "en": "That will affect how others see you"
+  },
+  {
+    "cn": "照别人说的去做",
+    "en": "Do as others say"
+  },
+  {
+    "cn": "转移你的注意力",
+    "en": "Divert your attention"
+  },
+  {
+    "cn": "你会失望的",
+    "en": "You will be disappointed"
+  },
+  {
+    "cn": "最好关注你的工作",
+    "en": "Better focus on your work"
+  },
+  {
+    "cn": "形式尚不明朗",
+    "en": "The form is still unclear"
+  },
+  {
+    "cn": "不要抱有成见",
+    "en": "Don't be biased"
+  },
+  {
+    "cn": "你必须现在就行动",
+    "en": "You must act now"
+  },
+  {
+    "cn": "那可能很难，但值得",
+    "en": "It may be difficult, but worth it"
+  },
+  {
+    "cn": "付出就会有回报",
+    "en": "Effort will be rewarded"
+  },
+  {
+    "cn": "对意外要有思想准备",
+    "en": "Be prepared for the unexpected"
+  },
+  {
+    "cn": "更细心地去倾听，你就会知道",
+    "en": "Listen more carefully and you will know"
+  },
+  {
+    "cn": "且行且思",
+    "en": "Act while thinking"
+  },
+  {
+    "cn": "你有能力以任何方式改善",
+    "en": "You have the ability to improve in any way"
+  },
+  {
+    "cn": "这是你不会忘记的事物",
+    "en": "This is something you won't forget"
+  },
+  {
+    "cn": "履行你的义务",
+    "en": "Fulfill your duty"
+  },
+  {
+    "cn": "快刀斩乱麻",
+    "en": "Cut the knot quickly"
+  },
+  {
+    "cn": "别在这上面下赌注",
+    "en": "Don't bet on this"
+  },
+  {
+    "cn": "也许吧",
+    "en": "Maybe"
+  },
+  {
+    "cn": "专注于你的家庭生活",
+    "en": "Focus on your family life"
+  },
+  {
+    "cn": "绝对不",
+    "en": "Absolutely not"
+  },
+  {
+    "cn": "等待",
+    "en": "Wait"
+  },
+  {
+    "cn": "别犯傻了",
+    "en": "Don't be foolish"
+  },
+  {
+    "cn": "可能吧",
+    "en": "Maybe"
+  },
+  {
+    "cn": "表示怀疑",
+    "en": "Express doubt"
+  },
+  {
+    "cn": "意义非凡",
+    "en": "Extraordinary significance"
+  },
+  {
+    "cn": "那可能非同寻常",
+    "en": "That may be out of the ordinary"
+  },
+  {
+    "cn": "不可能失败",
+    "en": "Cannot fail"
+  },
+  {
+    "cn": "你需要了解更多",
+    "en": "You need to know more"
+  },
+  {
+    "cn": "情况很快就会有变化",
+    "en": "The situation will change soon"
+  },
+  {
+    "cn": "这并不重要",
+    "en": "It's not important"
+  },
+  {
+    "cn": "顺其自然",
+    "en": "Let it be natural"
+  },
+  {
+    "cn": "问问你最好的朋友",
+    "en": "Ask your best friend"
+  },
+  {
+    "cn": "这时不要再自找麻烦",
+    "en": "Don't be so selfish"
+  },
+  {
+    "cn": "晚一点处理",
+    "en": "Handle it later"
+  },
+  {
+    "cn": "尝试一种更可能的解决方案",
+    "en": "Try a more likely solution"
+  },
+  {
+    "cn": "先做重要的事",
+    "en": "Do the important things first"
+  },
+  {
+    "cn": "投硬币来做决定吧",
+    "en": "Toss a coin to decide"
+  },
+  {
+    "cn": "这也取决于另一种情况",
+    "en": "It also depends on another situation"
+  },
+  {
+    "cn": "你最终能如愿",
+    "en": "You will eventually get what you want"
+  },
+  {
+    "cn": "可行",
+    "en": "Feasible"
+  },
+  {
+    "cn": "答案就在你家窗外",
+    "en": "The answer is right outside your window"
+  },
+  {
+    "cn": "现在的你比以往任何时候都清楚",
+    "en": "You are clearer now than ever before"
+  },
+  {
+    "cn": "只需说声\"谢谢\"",
+    "en": "Just say \"Thank you\""
+  },
+  {
+    "cn": "或许，等你再年长些就明白了",
+    "en": "Perhaps, you'll understand when you're older"
+  },
+  {
+    "cn": "这将轰动一时",
+    "en": "This will cause a sensation"
+  },
+  {
+    "cn": "放手一搏",
+    "en": "Take a chance"
+  },
+  {
+    "cn": "事情会朝目标发展",
+    "en": "Things will develop toward the goal"
+  },
+  {
+    "cn": "更细心去了解，你就知道该怎么做了",
+    "en": "With more careful understanding, you'll know what to do"
+  },
+  {
+    "cn": "需要做更多的努力",
+    "en": "More effort is needed"
+  },
+  {
+    "cn": "等待一个更好的机会",
+    "en": "Wait for a better opportunity"
+  },
+  {
+    "cn": "数到5，再试一次",
+    "en": "Count to 5, then try again"
+  },
+  {
+    "cn": "你不得不妥协",
+    "en": "You have to compromise"
+  },
+  {
+    "cn": "很快就能解决",
+    "en": "It will be resolved soon"
+  },
+  {
+    "cn": "十分确定",
+    "en": "Very certain"
+  },
+  {
+    "cn": "这还不确定",
+    "en": "This is still uncertain"
+  },
+  {
+    "cn": "谨慎处理",
+    "en": "Handle with caution"
+  },
+  {
+    "cn": "全力以赴",
+    "en": "Give it your all"
+  },
+  {
+    "cn": "重新考虑你的做法",
+    "en": "Reconsider your approach"
+  },
+  {
+    "cn": "问问你的母亲吧",
+    "en": "Ask your mother"
+  },
+  {
+    "cn": "如果你独自一人就不要",
+    "en": "Don't, if you're alone"
+  },
+  {
+    "cn": "无需担忧",
+    "en": "No need to worry"
+  },
+  {
+    "cn": "保持开放的心态",
+    "en": "Keep an open mind"
+  },
+  {
+    "cn": "你会为自己所做的感到高兴的",
+    "en": "You will be glad for what you did"
+  },
+  {
+    "cn": "发挥你的想象力",
+    "en": "Use your imagination"
+  },
+  {
+    "cn": "献出你的一切",
+    "en": "Give your all"
+  },
+  {
+    "cn": "顺从你的意愿",
+    "en": "Follow your will"
+  },
+  {
+    "cn": "先做好自己的事",
+    "en": "Take care of your own matters first"
+  },
+  {
+    "cn": "不要怀疑",
+    "en": "Don't doubt"
+  },
+  {
+    "cn": "是时候做新打算了​",
+    "en": "It's time to make new plans"
+  },
+  {
+    "cn": "省省力气吧",
+    "en": "Save your energy"
+  },
+  {
+    "cn": "合作将是关键",
+    "en": "Cooperation will be the key"
+  },
+  {
+    "cn": "此时不宜",
+    "en": "Not advisable at this time"
+  },
+  {
+    "cn": "把这看做一次机会",
+    "en": "See this as an opportunity"
+  },
+  {
+    "cn": "莫等待",
+    "en": "Don't wait"
+  },
+  {
+    "cn": "你可能不得不放弃其他东西",
+    "en": "You may have to give up other things"
+  },
+  {
+    "cn": "遵守规则",
+    "en": "Follow the rules"
+  },
+  {
+    "cn": "相关问题可能会出现",
+    "en": "Related issues may arise"
+  },
+  {
+    "cn": "事情将如你所愿",
+    "en": "Things will be as you wish"
+  },
+  {
+    "cn": "赌一把",
+    "en": "Take a chance"
+  },
+  {
+    "cn": "以后再处理",
+    "en": "Handle it later"
+  },
+  {
+    "cn": "结果是乐观的",
+    "en": "The outcome is optimistic"
+  },
+  {
+    "cn": "期待解决",
+    "en": "Expect a resolution"
+  },
+  {
+    "cn": "灵活应对",
+    "en": "Be flexible"
+  },
+  {
+    "cn": "注意细节",
+    "en": "Pay attention to details"
+  },
+  {
+    "cn": "你的行动会使一切变好",
+    "en": "Your actions will make everything better"
+  },
+  {
+    "cn": "答案就在公园里",
+    "en": "The answer is in the park"
+  },
+  {
+    "cn": "消除你自身的障碍",
+    "en": "Remove your own obstacles"
+  },
+  {
+    "cn": "这是不明智的",
+    "en": "This is unwise"
+  },
+  {
+    "cn": "将需要大量的努力",
+    "en": "It will require a lot of effort"
+  },
+  {
+    "cn": "不要勉强自己",
+    "en": "Don't force yourself"
+  },
+  {
+    "cn": "是时候做打算了",
+    "en": "It's time to make plans"
+  },
+  {
+    "cn": "别再犹豫了",
+    "en": "Don't hesitate any longer"
+  },
+  {
+    "cn": "享受这次体验",
+    "en": "Enjoy this experience"
+  },
+  {
+    "cn": "要付出坚持不懈的努力",
+    "en": "Persevering effort is required"
+  },
+  {
+    "cn": "那仍旧无法预测",
+    "en": "It is still unpredictable"
+  },
+  {
+    "cn": "毋庸置疑",
+    "en": "Without a doubt"
+  },
+  {
+    "cn": "多花点时间来做决定",
+    "en": "Take more time to decide"
+  },
+  {
+    "cn": "只做这一次",
+    "en": "Do it just this once"
+  },
+  {
+    "cn": "这是不明智的",
+    "en": "This is unwise"
+  },
+  {
+    "cn": "做些改变",
+    "en": "Make some changes"
+  },
+  {
+    "cn": "可行",
+    "en": "Feasible"
+  },
+  {
+    "cn": "先做好其他事",
+    "en": "Take care of other things first"
+  },
+  {
+    "cn": "不要陷入到情绪之中",
+    "en": "Don't get caught up in emotions"
+  },
+  {
+    "cn": "相信你的直觉",
+    "en": "Trust your intuition"
+  },
+  {
+    "cn": "采纳智者的建议",
+    "en": "Heed the advice of the wise"
+  },
+  {
+    "cn": "情况不明了",
+    "en": "The situation is unclear"
+  },
+  {
+    "cn": "你不得不妥协",
+    "en": "You have to compromise"
+  },
+  {
+    "cn": "列出否定的理由",
+    "en": "List the reasons against it"
+  },
+  {
+    "cn": "要有耐心",
+    "en": "Have patience"
+  },
+  {
+    "cn": "一笑置之",
+    "en": "Laugh it off"
+  },
+  {
+    "cn": "继续",
+    "en": "Continue"
+  },
+  {
+    "cn": "你必须随机应变",
+    "en": "You must adapt as you go"
+  },
+  {
+    "cn": "别忘记享受乐趣",
+    "en": "Don't forget to enjoy yourself"
+  },
+  {
+    "cn": "那是在浪费金钱",
+    "en": "That is a waste of money"
+  },
+  {
+    "cn": "重要的优先",
+    "en": "Prioritize what's important"
+  },
+  {
+    "cn": "为了做出最好的决定，务必保持冷静",
+    "en": "Stay calm to make the best decision"
+  },
+  {
+    "cn": "尝试一个更没把握的方法",
+    "en": "Try a less certain approach"
+  },
+  {
+    "cn": "清除你自身的障碍",
+    "en": "Remove your own obstacles"
+  },
+  {
+    "cn": "那可能已成事实",
+    "en": "It may already be a fact"
+  },
+  {
+    "cn": "保守你的秘密",
+    "en": "Keep your secret"
+  },
+  {
+    "cn": "你必须马上行动",
+    "en": "You must act immediately"
+  },
+  {
+    "cn": "不要妄下赌注",
+    "en": "Don't bet recklessly"
+  },
+  {
+    "cn": "那可能已无法改变",
+    "en": "It may be unchangeable"
+  },
+  {
+    "cn": "一些帮助能确保你成功",
+    "en": "Some help can ensure your success"
+  },
+  {
+    "cn": "你肯定会获得支持",
+    "en": "You will definitely get support"
+  },
+  {
+    "cn": "只做一次",
+    "en": "Do it just once"
+  },
+  {
+    "cn": "遵循智者的建议",
+    "en": "Follow the advice of the wise"
+  },
+  {
+    "cn": "如你所愿",
+    "en": "As you wish"
+  },
+  {
+    "cn": "当局者迷",
+    "en": "It's unclear to those involved"
+  },
+  {
+    "cn": "无论你怎么做，结果依旧",
+    "en": "No matter what you do, the result is the same"
+  },
+  {
+    "cn": "先主后次",
+    "en": "Prioritize the main over the secondary"
+  },
+  {
+    "cn": "这会让你付出代价",
+    "en": "This will cost you"
+  },
+  {
+    "cn": "尽早行动",
+    "en": "Act sooner rather than later"
+  },
+  {
+    "cn": "寻求更多选择",
+    "en": "Seek more choices"
+  },
+  {
+    "cn": "你现在比以往任何时候都清楚",
+    "en": "You are clearer now than ever before"
+  },
+  {
+    "cn": "极可能发生事故",
+    "en": "An accident is very likely"
+  },
+  {
+    "cn": "带着好奇去探索",
+    "en": "Explore with curiosity"
+  },
+  {
+    "cn": "列出这样做的理由",
+    "en": "List the reasons to do it"
+  },
+  {
+    "cn": "马上停下来",
+    "en": "Stop right away"
+  },
+  {
+    "cn": "这不是很确定",
+    "en": "This is not very certain"
+  },
+  {
+    "cn": "不用担心",
+    "en": "No need to worry"
+  },
+  {
+    "cn": "不要告诉别人",
+    "en": "Don't tell others"
+  },
+  {
+    "cn": "你需要其他人的帮助",
+    "en": "You need help from others"
+  },
+  {
+    "cn": "那将是一件乐事",
+    "en": "That will be a pleasure"
+  },
+  {
+    "cn": "不要迫于压力草率行事",
+    "en": "Don't act rashly under pressure"
+  },
+  {
+    "cn": "不要等待",
+    "en": "Don't wait"
+  },
+  {
+    "cn": "你能以任何方式改善现状",
+    "en": "You can improve the situation in any way"
+  },
+  {
+    "cn": "你会为此感到高兴",
+    "en": "You will be glad for this"
+  },
+  {
+    "cn": "放弃之前的想法",
+    "en": "Abandon your previous idea"
+  },
+  {
+    "cn": "你不会忘记这些",
+    "en": "You won't forget this"
+  },
+  {
+    "cn": "谨慎对待",
+    "en": "Treat with caution"
+  },
+  {
+    "cn": "放弃你现在的想法",
+    "en": "Abandon your current idea"
+  },
+  {
+    "cn": "有理由保持乐观",
+    "en": "There is reason to be optimistic"
+  },
+  {
+    "cn": "你会发现自己难以妥协",
+    "en": "You will find it hard to compromise"
+  },
+  {
+    "cn": "改变不会很快发生",
+    "en": "Change will not happen quickly"
+  },
+  {
+    "cn": "有些障碍需要克服",
+    "en": "Some obstacles need to be overcome"
+  },
+  {
+    "cn": "耐心点",
+    "en": "Be patient"
+  },
+  {
+    "cn": "果断放弃",
+    "en": "Abandon decisively"
+  },
+  {
+    "cn": "最好把心思放在工作上",
+    "en": "Better focus on your work"
+  },
+  {
+    "cn": "要做就做好，否则就不要去做",
+    "en": "Do it well or don't do it at all"
+  },
+  {
+    "cn": "深表怀疑",
+    "en": "Express deep doubt"
+  },
+  {
+    "cn": "最好的解决方法可能不太明显",
+    "en": "The best solution may not be obvious"
+  },
+  {
+    "cn": "已超出你的控制",
+    "en": "It is beyond your control"
+  },
+  {
+    "cn": "看看会发生什么",
+    "en": "See what happens"
+  },
+  {
+    "cn": "你需要更多信息",
+    "en": "You need more information"
+  },
+  {
+    "cn": "开阔视野",
+    "en": "Broaden your perspective"
+  },
+  {
+    "cn": "看得更清楚些",
+    "en": "See more clearly"
+  },
+  {
+    "cn": "结果可能会令人震惊",
+    "en": "The result may be shocking"
+  },
+  {
+    "cn": "节省你的精力吧",
+    "en": "Save your energy"
+  },
+  {
+    "cn": "管它呢",
+    "en": "Never mind it"
+  },
+  {
+    "cn": "无论你做什么，结果依旧",
+    "en": "No matter what you do, the result is the same"
+  },
+  {
+    "cn": "那将引起一些纷争",
+    "en": "That will cause some disputes"
+  },
+  {
+    "cn": "相关问题可能会浮出水面",
+    "en": "Related issues may surface"
+  },
+  {
+    "cn": "遵循其他人的建议",
+    "en": "Follow others' advice"
+  },
+  {
+    "cn": "改变将不会很快发生",
+    "en": "Change will not happen quickly"
+  },
+  {
+    "cn": "值得付出努力",
+    "en": "It's worth the effort"
+  },
+  {
+    "cn": "负起责任来",
+    "en": "Take responsibility"
+  },
+  {
+    "cn": "不值得一争",
+    "en": "Not worth fighting for"
+  },
+  {
+    "cn": "问问你的父亲吧",
+    "en": "Ask your father"
+  },
+  {
+    "cn": "向别人倾诉",
+    "en": "Confide in others"
+  },
+  {
+    "cn": "看起来还行",
+    "en": "Looks okay"
+  },
+  {
+    "cn": "绝不",
+    "en": "Absolutely not"
+  },
+  {
+    "cn": "等待一个更好的提议",
+    "en": "Wait for a better proposal"
+  },
+  {
+    "cn": "你可能会遭遇反对",
+    "en": "You may encounter opposition"
+  },
+  {
+    "cn": "告诉某人那对你意味着什么",
+    "en": "Tell someone what it means to you"
+  },
+  {
+    "cn": "一切都取决于你的选择",
+    "en": "Everything depends on your choice"
+  },
+  {
+    "cn": "把这看作一个时机",
+    "en": "See this as an opportunity"
+  },
+  {
+    "cn": "不要陷入你的情绪",
+    "en": "Don't get caught up in your emotions"
+  },
+  {
+    "cn": "你一定得这么做",
+    "en": "You must do it this way"
+  },
+  {
+    "cn": "享受这个过程",
+    "en": "Enjoy the process"
+  },
+  {
+    "cn": "不识庐山真面目，只缘身在此山中",
+    "en": "You can't see the true face of Lushan, only because you're in the mountain"
+  },
+  {
+    "cn": "你不会失望的",
+    "en": "You won't be disappointed"
+  },
+  {
+    "cn": "是时候走了",
+    "en": "It's time to leave"
+  },
+  {
+    "cn": "欣然接受",
+    "en": "Accept with pleasure"
+  },
+  {
+    "cn": "遵循其他人的意见",
+    "en": "Follow others' opinions"
+  },
+  {
+    "cn": "说出来吧",
+    "en": "Say it out loud"
+  },
+  {
+    "cn": "慷慨大度一些",
+    "en": "Be more generous"
+  },
+  {
+    "cn": "你可能遭遇反对",
+    "en": "You may encounter opposition"
+  },
+  {
+    "cn": "你能否不要抗拒",
+    "en": "Could you not resist"
+  },
+  {
+    "cn": "要障碍需要克服",
+    "en": "Obstacles need to be overcome"
+  },
+  {
+    "cn": "做出改变",
+    "en": "Make changes"
+  },
+  {
+    "cn": "无法保证",
+    "en": "No guarantee"
+  },
+  {
+    "cn": "算了吧",
+    "en": "Forget about it"
+  },
+  {
+    "cn": "合作是关键",
+    "en": "Cooperation is key"
+  }
+];
